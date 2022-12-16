a4e8bd677f0aec292bf5f9980a96f2496b12b27e
Ahnaf An Nafee <ahnafnafee@gmail.com>
2022-12-16 10:05:02 -0500

    chore: fixed pd

--- src/components/UI/templates/Hero.tsx
+++ src/components/UI/templates/Hero.tsx
@@ -8,7 +8,7 @@ export type HeroProps = {
 }
 
 export const Hero: React.FunctionComponent<HeroProps> = (props) => (
-  <section className={twclsx('content-auto pb-8 md:pb-16', props.className)}>
+  <section className={twclsx('content-auto pt-4 md:pt-8 pb-8 md:pb-16', props.className)}>
     <h1 className='mb-3 md:mb-4'>{props.title}</h1>
     <p>{props.description}</p>
     {props.children}
